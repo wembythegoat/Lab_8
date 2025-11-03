9ed7c207cd68ac18c516dc1a5919b160484b55af
Aswinth Sinnathamby <aswinthsinnathamby@gmail.com>
2025-11-03 09:40:39 -0500

    Add files via upload

--- LAB_8/src/main/java/com/mycompany/lab_8/PrimaryController.java
+++ LAB_8/src/main/java/com/mycompany/lab_8/PrimaryController.java
@@ -1,12 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package com.mycompany.lab_8;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
-public class PrimaryController {
+/**
+ * FXML Controller class
+ *
+ * @author 2472557
+ */
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    private BorderPane root;
+    @FXML
+    private VBox sidebar;
+    @FXML
+    private RadioButton idBlack;
+    @FXML
+    private RadioButton idRed;
+    @FXML
+    private RadioButton idBlue;
+    @FXML
+    private RadioButton idGreen;
+    @FXML
+    private RadioButton idSmall;
+    @FXML
+    private RadioButton idMedium;
+    @FXML
+    private RadioButton idLarge;
+    @FXML
+    private Pane drawingAreaPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
 }
--- LAB_8/src/main/java/module-info.java
+++ LAB_8/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.lab_8 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.lab_8 to javafx.fxml;
     exports com.mycompany.lab_8;
