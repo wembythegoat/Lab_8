897c7e3cee2f5cd13f61446e10617bd31ea8c9c9
Aswinth Sinnathamby <aswinthsinnathamby@gmail.com>
2025-11-03 10:34:18 -0500

    The lab is completed. All the event handlers done

--- LAB_8/src/main/java/com/mycompany/lab_8/PrimaryController.java
+++ LAB_8/src/main/java/com/mycompany/lab_8/PrimaryController.java
@@ -11,11 +11,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 /**
  * FXML Controller class
@@ -53,32 +58,84 @@ public class PrimaryController implements Initializable {
     @FXML
     private Button clear;
 
+    private PenSize radius = PenSize.MEDIUM;
+    private Paint brushColor = Color.BLACK;
+
+    private enum PenSize {
+        SMALL(2),
+        MEDIUM(4),
+        LARGE(6);
+
+        private final int radius;
+
+        PenSize(int radius) {
+            this.radius = radius;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        idBlack.setUserData(Color.BLACK);
+        idRed.setUserData(Color.RED);
+        idBlue.setUserData(Color.BLUE);
+        idGreen.setUserData(Color.GREEN);
+        idSmall.setUserData(PenSize.SMALL);
+        idMedium.setUserData(PenSize.MEDIUM);
+        idLarge.setUserData(PenSize.LARGE);
+    }
 
     @FXML
     private void colorRadioButtonSelected(ActionEvent event) {
+        Toggle selectedToggle = colorToggleGroup.getSelectedToggle();
+        
+        if (selectedToggle != null) {
+            Object data = selectedToggle.getUserData();
+            brushColor = (Color) data;
+        }
     }
 
     @FXML
     private void sizeRadioButtonSelected(ActionEvent event) {
+        Toggle selectedToggle = sizeToggleGroup.getSelectedToggle();
+        
+        if(selectedToggle != null) {
+            Object data = selectedToggle.getUserData();
+            radius = (PenSize) data;
+        }
+        
     }
 
     @FXML
     private void undoButtonPresssed(ActionEvent event) {
+        int count = drawingAreaPane.getChildren().size();
+        
+        if (count > 0) {
+            drawingAreaPane.getChildren().remove(count - 1);
+        }
     }
 
     @FXML
     private void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
     }
 
     @FXML
     private void drawingAreaMouseDragged(MouseEvent event) {
+        Circle circle = new Circle();
+        circle.setLayoutX(event.getX());
+        circle.setLayoutY(event.getY());
+        circle.setRadius(radius.getRadius());
+        circle.setFill(brushColor);
+        drawingAreaPane.getChildren().addAll(circle);
+        
+        // ADD OBSERVABLE CIRCLE
     }
-    
+
 }
